ba429f797cef2945db25c7a0afa5985bf18729ff
bhil82 <bi-ratan@hotmail.com>
2022-05-20 19:53:31 -0300

    primeiro commit

---
+++ exercicio.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="Assets/css/estilo.css">
+    <link rel="stylesheet" href="Assets/css/exercicio.css">
+    <title>Exercício</title>
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercício</h2>
+    </header>
+    <nav class="navegacao">      
+
+    <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+            
+        <?php
+        
+    /*  include($_GET['dir'] . "/" . $_GET['file'] . ".php");
+
+        include("{$_GET['dir']}/{$_GET['file']}.php");
+        
+        include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+     */
+
+        ?>   
+         
+        </div>
+    </main>
+    <footer class="rodape">
+        Ubiratan <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="Assets/css/estilo.css">
+    <link rel="stylesheet" href="Assets/css/exercicio.css">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <h3>Módulo 01</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        Ubiratan © <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
