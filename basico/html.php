77f0788ced937c70d368e0efd55e67e313dc5137
bhil82 <bi-ratan@hotmail.com>
2022-05-21 17:05:40 -0300

    sincro

---
+++ basico/html.php
@@ -0,0 +1,13 @@
+<div class="titulo">integração html</div>
+<h1>
+    <?php
+    echo 'olá';
+    echo'<small>';
+    echo'mundo';
+    echo'</small>';
+    ?>
+</h1>
+<?= "<div> Outra forma de me 'expressar'!</div> "?>
+<br>
+<div><button><?= "legal" ?> </button></div>
+    
